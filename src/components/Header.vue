aa2151efa474cd4324ec6348b2b886596d9b4ec4
philusdevs <philusdevs@gmail.com>
2023-05-05 10:25:42 +0200

    update logo style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <header>
+    <header class="header">
       <a href="/" class="logo-link">
         <img :src="require('@/assets/logo.png')" alt="Website Logo" class="logo">
       </a>
@@ -13,21 +13,27 @@
   </script>
   
   <style>
- header {
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 1000;
-}
-
-.logo-link {
-  display: inline-block;
-  padding: 10px;
-}
-
-.logo {
-  height: 110px;
-  width: auto;
-}
-
+  .header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1000;
+    width: 100%;
+    height: 70px; 
+    background-color: #f8f9fa; 
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  .logo-link {
+    display: inline-block;
+    padding: 10px;
+  }
+  
+  .logo {
+    height: 80px; 
+    width: auto;
+  }
   </style>
+  
