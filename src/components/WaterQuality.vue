221aa796afa4c057349a1e9ec7c9d43a84041fbd
philusdevs <philusdevs@gmail.com>
2023-06-23 15:41:28 +0200

    Update Water Quality Checker component
    - Integrate API endpoint for fetching municipalities and their water quality data
    - Update data fetching and calculation methods to use the new API endpoint
    - Add comments to explain each section of the code
    
    The Water Quality Checker component now fetches the list of municipalities and their corresponding water quality data from the API. It dynamically populates the select dropdown with the available municipalities and calculates the safety status for disinfectant, non-health aesthetic, and operational categories based on the selected municipal's data.
    
    The code has also been thoroughly commented to improve code readability and maintainability.
    
    [API Endpoint]: https://drinkablesa-api-b93993490e91.herokuapp.com/municipalities

--- src/components/WaterQuality.vue
+++ src/components/WaterQuality.vue
@@ -1,117 +1,104 @@
 <template>
-    <div>
-      <h1>Water Quality Checker</h1>
-      <select v-model="selectedMunicipal" @change="calculateWaterSafety">
-        <option value="">Select a Municipal to Check Water Quality</option>
-        <option v-for="municipal in municipals" :key="municipal">{{ municipal }}</option>
-      </select>
-  
-      <div v-if="selectedMunicipal">
-        <h2>Water Quality for {{ selectedMunicipal }}</h2>
-        <div>
-          <h3>Disinfectant</h3>
-          <p :style="{ color: getSafetyColor('disinfectant') }">{{ disinfectantSafety }} {{ selectedMunicipalData.disinfectant }}</p>
-        </div>
-        <div>
-          <h3>Non-Health Aesthetic</h3>
-          <p :style="{ color: getSafetyColor('nonHealthAesthetic') }">{{ nonHealthAestheticSafety }} {{ selectedMunicipalData.non_health_aesthetic }}</p>
-        </div>
-        <div>
-          <h3>Operations</h3>
-          <p :style="{ color: getSafetyColor('operational') }">{{ operationalSafety }} {{ selectedMunicipalData.operational }}</p>
-        </div>
+  <div>
+    <h1>Water Quality Checker</h1>
+    <select v-model="selectedMunicipal" @change="calculateWaterSafety">
+      <option value="">Select a Municipal to Check Water Quality</option>
+      <option v-for="municipal in municipals" :key="municipal">{{ municipal }}</option>
+    </select>
+
+    <div v-if="selectedMunicipal">
+      <h2>Water Quality for {{ selectedMunicipal }}</h2>
+      <div>
+        <h3>Disinfectant</h3>
+        <p :style="{ color: getSafetyColor('disinfectant') }">{{ disinfectantSafety }} {{ selectedMunicipalData.disinfectant }}</p>
+      </div>
+      <div>
+        <h3>Non-Health Aesthetic</h3>
+        <p :style="{ color: getSafetyColor('nonHealthAesthetic') }">{{ nonHealthAestheticSafety }} {{ selectedMunicipalData.non_health_aesthetic }}</p>
+      </div>
+      <div>
+        <h3>Operations</h3>
+        <p :style="{ color: getSafetyColor('operational') }">{{ operationalSafety }} {{ selectedMunicipalData.operational }}</p>
       </div>
     </div>
+  </div>
 </template>
 
-  
-  <script>
-  import axios from 'axios';
-  import Papa from 'papaparse';
-  
-  export default {
-    name: 'WaterQuality',
-    //settting up data() function,with the required data properties
-    data() {
-      return {
-        municipals: [],
-        selectedMunicipal: '',
-        waterQualityData: [],
-        disinfectantSafety: '',
-        nonHealthAestheticSafety: '',
-        operationalSafety: '',
-        selectedMunicipalData: {},
-      };
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'WaterQuality',
+
+  data() {
+    return {
+      municipals: [],                 // Array to store the list of municipalities
+      selectedMunicipal: '',          // Currently selected municipal
+      waterQualityData: [],           // Array to store water quality data
+      disinfectantSafety: '',         // Safety status for disinfectant
+      nonHealthAestheticSafety: '',   // Safety status for non-health aesthetic
+      operationalSafety: '',          // Safety status for operational
+      selectedMunicipalData: {},      // Data of the selected municipal
+    };
+  },
+
+  methods: {
+    async fetchData() {
+      try {
+        // Fetch the list of municipalities and water quality data from the API
+        const response = await axios.get('https://drinkablesa-api-b93993490e91.herokuapp.com/municipalities');
+        this.waterQualityData = response.data.municipalities;
+        this.municipals = this.waterQualityData.map((item) => item.name);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
     },
-    methods: {
-        //Adds an object, to fetch the data from the GitHub CSV file and parse it:
-      async fetchData() {
-        const response = await axios.get('https://raw.githubusercontent.com/philusdevs/Drinkable-SA/main/Drinkable.csv');
-        const csvData = response.data;
-  
-        Papa.parse(csvData, {
-          header: true,
-          complete: (results) => {
-            this.waterQualityData = results.data.map((item) => {
-              return {
-                municipal: item.Municipal,
-                disinfectant: item.Disinfectant,
-                non_health_aesthetic: item['Non Health Aesthetic'],
-                operational: item.Operational,
-              };
-            });
-            this.municipals = Array.from(new Set(this.waterQualityData.map((item) => item.municipal)));
-          },
-        });
-      },
-  //// Measures the water safety based on the compliance percentage of each Municipal
 
-      calculateWaterSafety() {
-        const municipalData = this.waterQualityData.find(
-          (item) => item.municipal === this.selectedMunicipal
-        );
-        // Emit the event to the parent component
-        this.$emit('selectionMade');
+    async calculateWaterSafety() {
+      try {
+        // Fetch the specific data for the selected municipal from the API
+        const response = await axios.get(`https://drinkablesa-api-b93993490e91.herokuapp.com/municipalities/${this.selectedMunicipal}`);
+        this.selectedMunicipalData = response.data;
 
-        this.selectedMunicipalData = municipalData;
-  
-        this.disinfectantSafety = municipalData.disinfectant === "No Data" ? '' :
-          parseFloat(municipalData.disinfectant) < 97 ? 'Please boil water before drinking current compliance is at' : 'Drinkable';
-  
-        this.nonHealthAestheticSafety = municipalData.non_health_aesthetic === "No Data" ? 'No Data Available' :
-          parseFloat(municipalData.non_health_aesthetic) < 97 ? 'Water maybe affected in appearance, taste, and odor, but does not pose a health risk, current compliance is at' : 'Drinkable';
-  
-        this.operationalSafety = municipalData.operational === "No Data" ? 'No Data Available' :
-          parseFloat(municipalData.operational) >= 97 ? 'Good' : 'Municipal is not meeting the operations compliance standard of 97% current Municipal compliance is at';
-      },
+        // Calculate safety status based on the fetched data
+        this.disinfectantSafety = this.selectedMunicipalData.disinfectant === "No Data" ? '' :
+          parseFloat(this.selectedMunicipalData.disinfectant) < 97 ? 'Please boil water before drinking. Current compliance is at' : 'Drinkable';
 
-      
-  
-getSafetyColor(safetyCategory) {
-  let safetyStatus = '';
+        this.nonHealthAestheticSafety = this.selectedMunicipalData.non_health_aesthetic === "No Data" ? 'No Data Available' :
+          parseFloat(this.selectedMunicipalData.non_health_aesthetic) < 97 ? 'Water may be affected in appearance, taste, and odor, but does not pose a health risk. Current compliance is at' : 'Drinkable';
 
-  switch (safetyCategory) {
-    case 'disinfectant':
-      safetyStatus = this.disinfectantSafety;
-      break;
-    case 'nonHealthAesthetic':
-      safetyStatus = this.nonHealthAestheticSafety;
-      break;
-    case 'operational':
-      safetyStatus = this.operationalSafety;
-      break;
-  }
+        this.operationalSafety = this.selectedMunicipalData.operational === "No Data" ? 'No Data Available' :
+          parseFloat(this.selectedMunicipalData.operational) >= 97 ? 'Good' : 'Municipal is not meeting the operations compliance standard of 97%. Current municipal compliance is at';
+      } catch (error) {
+        console.error('Error fetching municipal data:', error);
+      }
+    },
 
-  if (safetyStatus === '') {
-    return 'gray';
-  }
+    getSafetyColor(safetyCategory) {
+      // Get the safety status based on the safety category
+      let safetyStatus = '';
 
-  return safetyStatus === 'Good' || safetyStatus === 'Drinkable' ? 'green' : 'red';
-},
+      switch (safetyCategory) {
+        case 'disinfectant':
+          safetyStatus = this.disinfectantSafety;
+          break;
+        case 'nonHealthAesthetic':
+          safetyStatus = this.nonHealthAestheticSafety;
+          break;
+        case 'operational':
+          safetyStatus = this.operationalSafety;
+          break;
+      }
 
-},
+      if (safetyStatus === '') {
+        return 'gray';
+      }
 
-mounted() {
+      return safetyStatus === 'Good' || safetyStatus === 'Drinkable' ? 'green' : 'red';
+    },
+  },
+
+  mounted() {
     this.fetchData();
   },
 };
@@ -120,10 +107,10 @@ mounted() {
 <style>
 h1, h2, h3 {
   margin-bottom: 2rem;
- 
 }
-h1{
-    text-align: center;
+
+h1 {
+  text-align: center;
 }
 
 select {
